51fb0d64b3d193fb3735a839c060784e482bad2b
victor-lopezm-um-es <victor.lopezm@um.es>
2024-05-03 17:04:01 +0200

    Implementación del patrón Chain of Responsability

--- sgd.py
+++ sgd.py
@@ -2,6 +2,7 @@
 
 from functools import reduce
 
+# Patrón Strategy
 class Context:
     def __init__(self, strategy=None):
         self._strategy = strategy
@@ -51,3 +52,53 @@ class Maximos_y_Minimos(ComputoEstadistico):
         min = reduce(lambda x, y: x if x < y else y, data)
 
         return max, min
+    
+# Patrón Chain of Responsability
+class ManejadorTemperaturas:
+    def __init__(self, succesor=None):
+        self.succesor = succesor
+
+    def manejarTemperaturas(self, request, data):
+        pass
+
+class ManejadorTempEstadisticos(ManejadorTemperaturas):
+    def manejarTemperaturas(self, request, data):
+        if request.level == "TempEstadisticos":
+            contexto = Context()
+
+            contexto.establecerEstrategia(Media_y_DesvTip())
+            media, desv_tip = contexto.hacerAlgo(data)
+
+            contexto.establecerEstrategia(Cuantiles())
+            q1, q2, q3 = contexto.hacerAlgo(data)
+
+            contexto.establecerEstrategia(Maximos_y_Minimos())
+            max, min = contexto.hacerAlgo(data)
+
+            return media, desv_tip, q1, q2, q3, max, min
+
+        elif self.succesor:
+            return self.succesor.manejarTemperaturas(request, data)
+
+class ManejadorLimTemp(ManejadorTemperaturas):
+    def manejarTemperaturas(self, request, data):
+        umbral = 41
+        if request.level == "LimTemp":
+            ultima_temperatura = data[-1]
+            return ultima_temperatura > umbral
+
+        elif self.succesor:
+            return self.succesor.manejarTemperaturas(request, data)
+
+class ManejadorAumentoTemp(ManejadorTemperaturas):
+    def manejarTemperaturas(self, request, data):
+        delta_umbral = 2.5
+        if request.level == "AumentoTemp":
+            data_30s = data[7:]
+            return any(map(lambda x, y: abs(x - y)  > delta_umbral, data, data[1:] + [data[-1]]))
+        elif self.succesor:
+            return self.succesor.manejarTemperaturas(request, data)
+
+class Request:
+    def __init__(self, level):
+        self.level = level
